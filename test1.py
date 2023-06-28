a4ef2c28b1b075d74118af53d5980a3bd68e8b51
hoangnam1810 <hoangnam.7715.got@gmail.com>
2023-06-28 21:18:19 +0700

    first commit

---
+++ test1.py
@@ -0,0 +1,48 @@
+# print("hello world ")
+
+
+# ctrl + /
+# copy nhanh: shirft + alt + arrow down
+# copy: cuối dòng rồi ctrl + c
+# muốn sửa nội dung (giống nhau) ctrl+ D
+# muốn cmt 1 đoạn văn bản dài: """ """
+""" ví dụ...."""
+
+# các kiểu dữ liệu cơ bản
+"""
+int, float, string, complex 
+"""
+
+# a = 3
+# print("a thuoc du lieu: ",type (a))
+
+
+"""
+    bài tập:
+    - cho biết kiểu của các giá trị sau: -50, 4.98908, .5, 4+5j
+    - định nghĩa 2 biến num1, num2 có cùng giá trị 1
+    - nhập vào tên và tuổi và hiển thị ra màn hình, cách nhau bởi dấu phẩy
+    - nhập vào 1 số nguyên, số thực, chuỗi và in các giá trị của chúng trên các dòng riêng biệt
+"""
+
+'''cau1'''
+a= -50
+print("a la du lieu: ",type(a))
+b= 4.98908
+print("b la du lieu: ",type(b))
+c=.5
+print("c la du lieu: ",type(c))
+d= 4+5j
+print("d la du lieu: ",type(d))
+ 
+'''cau2'''
+num1=num2=1
+print("ok")
+
+'''cau3'''
+name = input("nhap ten: ")
+age = input("nhaptuoi:" )
+print("ten:{}, tuoi:{}".format(name,age))
+
+'''cau4'''
+
